d46247a64d39d73e94c8c60d6786ec096639b95c
Esherymack <esherymack@gmail.com>
2019-03-16 20:12:08 -0400

    Added initial main page files

---
+++ CS320_Team_Fractal_Website/src/edu/ycp/cs320/CS320_Team_Fractal_Website/servlet/MainPageServlet.java
@@ -0,0 +1,21 @@
+package edu.ycp.cs320.CS320_Team_Fractal_Website.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MainPageServlet extends HttpServlet
+{
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+		throws ServletException, IOException
+	{
+		System.out.println("Main Page Servlet: doGet");
+		req.getRequestDispatcher("/_view/mainPage.jsp").forward(req,  resp);	
+	}
+}
---
+++ CS320_Team_Fractal_Website/war/_view/mainPage.jsp
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>Fractal Main</title>
+	</head>
+	
+	<body>
+		<p>Main page of the fractal site</p>
+	</body>
+</html>
