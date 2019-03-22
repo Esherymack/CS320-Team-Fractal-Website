53c86f905cf4b731e003d4c52a1287fab9ff3fce
Zachary-Ronayne <zronayne42@gmail.com>
2019-03-22 19:33:18 -0400

    Moved image saving to FractalController

--- CS320_Team_Fractal_Website/src/edu/ycp/cs320/CS320_Team_Fractal_Website/controller/FractalController.java
+++ CS320_Team_Fractal_Website/src/edu/ycp/cs320/CS320_Team_Fractal_Website/controller/FractalController.java
@@ -1,5 +1,14 @@
 package edu.ycp.cs320.CS320_Team_Fractal_Website.controller;
 
+import java.awt.image.BufferedImage;
+import java.io.BufferedOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.imageio.ImageIO;
+
 /**
  * A generic controller for a fractal controller
  */
@@ -10,5 +19,28 @@ public abstract class FractalController{
 	 * @return true if the fractal was rendered, false otherwise
 	 */
 	public abstract boolean render();
+	
+	/**
+	 * Sends the given image to be saved in the database
+	 * @param img
+	 */
+	public void sendImage(BufferedImage img){
+		OutputStream os = null;
+		try{
+			os = new BufferedOutputStream(new FileOutputStream("./war/img/result.png"));
+			ImageIO.write(img, "PNG", os);
+		}catch (FileNotFoundException e){
+			e.printStackTrace();
+		}catch (IOException e){
+			e.printStackTrace();
+		}
+		if(os != null){
+			try{
+				os.close();
+			}catch (IOException e){
+				e.printStackTrace();
+			}
+		}
+	}
 	
 }
--- CS320_Team_Fractal_Website/src/edu/ycp/cs320/CS320_Team_Fractal_Website/controller/MandelbrotController.java
+++ CS320_Team_Fractal_Website/src/edu/ycp/cs320/CS320_Team_Fractal_Website/controller/MandelbrotController.java
@@ -3,13 +3,6 @@ package edu.ycp.cs320.CS320_Team_Fractal_Website.controller;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
-import java.io.BufferedOutputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-
-import javax.imageio.ImageIO;
 
 import edu.ycp.cs320.CS320_Team_Fractal_Website.model.Mandelbrot;
 import edu.ycp.cs320.CS320_Team_Fractal_Website.renderer.Complex;
@@ -29,22 +22,7 @@ public class MandelbrotController extends FractalController{
 			@Override
 			public void run(){
 				BufferedImage img = renderIterCounts();
-				OutputStream os = null;
-				try{
-					os = new BufferedOutputStream(new FileOutputStream("./war/img/result.png"));
-					ImageIO.write(img, "PNG", os);
-				}catch (FileNotFoundException e){
-					e.printStackTrace();
-				}catch (IOException e){
-					e.printStackTrace();
-				}
-				if(os != null)
-					try{
-						os.close();
-					}catch (IOException e){
-						e.printStackTrace();
-					}
-				
+				sendImage(img);
 			}
 		});
 		
