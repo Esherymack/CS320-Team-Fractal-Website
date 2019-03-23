39fcbc3f33464fa2d928b43250159f8d9055d651
Zachary-Ronayne <zronayne42@gmail.com>
2019-03-22 20:07:01 -0400

    Squashed bug with output stream not closing

--- CS320_Team_Fractal_Website/src/edu/ycp/cs320/CS320_Team_Fractal_Website/controller/FractalController.java
+++ CS320_Team_Fractal_Website/src/edu/ycp/cs320/CS320_Team_Fractal_Website/controller/FractalController.java
@@ -2,6 +2,7 @@ package edu.ycp.cs320.CS320_Team_Fractal_Website.controller;
 
 import java.awt.image.BufferedImage;
 import java.io.BufferedOutputStream;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -25,22 +26,28 @@ public abstract class FractalController{
 	 * @param img
 	 */
 	public void sendImage(BufferedImage img){
-		OutputStream os = null;
 		try{
-			os = new BufferedOutputStream(new FileOutputStream("./war/img/result.png"));
-			ImageIO.write(img, "PNG", os);
-		}catch (FileNotFoundException e){
-			e.printStackTrace();
-		}catch (IOException e){
+			ImageIO.write(img, "PNG", new File("./war/img/result.png"));
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		if(os != null){
-			try{
-				os.close();
-			}catch (IOException e){
-				e.printStackTrace();
-			}
-		}
+		
+//		OutputStream os = null;
+//		try{
+//			os = new BufferedOutputStream(new FileOutputStream("./war/img/result.png"));
+//			ImageIO.write(img, "PNG", os);
+//		}catch (FileNotFoundException e){
+//			e.printStackTrace();
+//		}catch (IOException e){
+//			e.printStackTrace();
+//		}
+//		if(os != null){
+//			try{
+//				os.close();
+//			}catch (IOException e){
+//				e.printStackTrace();
+//			}
+//		}
 	}
 	
 }
