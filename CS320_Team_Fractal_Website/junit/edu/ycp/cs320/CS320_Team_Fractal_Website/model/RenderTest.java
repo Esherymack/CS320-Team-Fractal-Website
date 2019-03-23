04ee3c2e937c51b5e76615d977fe81520a479a02
Madison Tibbett <Esherymack@users.noreply.github.com>
2019-03-23 13:34:14 -0400

    Added render tests

---
+++ CS320_Team_Fractal_Website/junit/edu/ycp/cs320/CS320_Team_Fractal_Website/model/RenderTest.java
@@ -0,0 +1,46 @@
+package edu.ycp.cs320.CS320_Team_Fractal_Website.model;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.ycp.cs320.CS320_Team_Fractal_Website.controller.MandelbrotController;
+import edu.ycp.cs320.CS320_Team_Fractal_Website.controller.SierpinskiController;
+import edu.ycp.cs320.CS320_Team_Fractal_Website.model.Mandelbrot;
+import edu.ycp.cs320.CS320_Team_Fractal_Website.model.Sierpinski;
+
+public class RenderTest 
+{
+	private Sierpinski sierModel;
+	private Mandelbrot mandelModel;
+	private SierpinskiController sierController;
+	private MandelbrotController mandelController;
+	
+	@Before
+	public void setUp()
+	{
+		sierModel = new Sierpinski();
+		sierController = new SierpinskiController(sierModel);
+		
+		mandelModel = new Mandelbrot();
+		mandelController = new MandelbrotController(mandelModel);
+		
+		sierModel.setLevel(8);
+	}
+	
+	@Test
+	public void sierpinskiRenderedTest()
+	{
+		boolean isRendered = true;
+		assertEquals(sierController.render(), isRendered);
+	}
+	
+	@Test
+	public void mandelbrotRenderedTest()
+	{
+		boolean isRendered = true;
+		assertEquals(mandelController.render(), isRendered);
+	}
+	
+}
