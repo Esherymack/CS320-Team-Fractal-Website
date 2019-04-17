50f82924ddddc2adcfd0395054e3c878cc281d6f
Zachary-Ronayne <zronayne42@gmail.com>
2019-04-16 20:10:53 -0400

    Modifications to the Koch controller

--- CS320_Team_Fractal_Website/src/edu/ycp/cs320/CS320_Team_Fractal_Website/controller/fractal/KochController.java
+++ CS320_Team_Fractal_Website/src/edu/ycp/cs320/CS320_Team_Fractal_Website/controller/fractal/KochController.java
@@ -2,6 +2,7 @@ package edu.ycp.cs320.CS320_Team_Fractal_Website.controller.fractal;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
 
 import edu.ycp.cs320.CS320_Team_Fractal_Website.model.fractal.Fractal;
@@ -52,12 +53,30 @@ public class KochController extends FractalController{
 		}
 		return true;
 	}
+	
+	public void renderKochLine(Point2D.Double start, Point2D.Double end, int iterations){
+		if(iterations <= 0) return;
+		
+		
+	}
+	
+	/**
+	 * Gets a point that is one third the distance from start to end
+	 * @param start the start point
+	 * @param end the end point
+	 * @return the point one third away
+	 */
+	public Point2D.Double getThirdPoint(Point2D.Double start, Point2D.Double end){
+		return new Point2D.Double(
+				start.x + (start.x - end.x) / 3,
+				start.y + (start.y - end.y) / 3);
+	}
 
 	@Override
 	public boolean acceptParameters(String[] params){
 		try{
 			model.setIterations(Integer.parseInt(params[0]));
-		}catch(NumberFormatException e){
+		}catch(NumberFormatException | ArrayIndexOutOfBoundsException e){
 			return false;
 		}
 		
