3d0c5d8d6d27ee47e8182953eec2c1e22fbae112
Esherymack <esherymack@gmail.com>
2019-04-23 17:32:11 -0400

    Fixed site such that if a user visits having had an account that no longer exists, they will be 'logged out'

--- CS320_Team_Fractal_Website/src/edu/ycp/cs320/CS320_Team_Fractal_Website/servlet/LandingPageServlet.java
+++ CS320_Team_Fractal_Website/src/edu/ycp/cs320/CS320_Team_Fractal_Website/servlet/LandingPageServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import edu.ycp.cs320.CS320_Team_Fractal_Website.controller.pages.CheckUserValidController;
 
 public class LandingPageServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -14,6 +15,8 @@ public class LandingPageServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
+		
+		CheckUserValidController isValidUser = new CheckUserValidController();
 		
 		System.out.println("Landing Page Servlet: doGet");
 		
@@ -25,18 +28,28 @@ public class LandingPageServlet extends HttpServlet {
 		if(cookies != null){
 			for(Cookie cookie : cookies)
 			{
-				if(cookie.getName().equals("user")) userName = cookie.getValue();
+				if(cookie.getName().equals("user"))
+				{
+					userName = cookie.getValue();
+				}
 			}
 		}
 		
-		// otherwise
-		String currentlyLoggedInMessage = "Currently logged in as " + userName;
-			
-		req.setAttribute("userName", userName);
-		req.setAttribute("currentlyLoggedInMessage", currentlyLoggedInMessage);
-		
+		// If a cookie is found, **make sure it is a valid cookie**
+		// That is, check and see if a username is found in the db that matches the cookie.
+		if(isValidUser.getUserIfExists(userName))
+		{
+			// otherwise
+			String currentlyLoggedInMessage = "Currently logged in as " + userName;
+			req.setAttribute("currentlyLoggedInMessage", currentlyLoggedInMessage);
+			req.setAttribute("userName", userName);
+		}
+		// Otherwise, just to clean up, delete the cookie of the deleted/nonexistent user ("log out").
+		else
+		{
+			isValidUser.LogOut(req, resp, "landingPage");
+		}
 		req.getRequestDispatcher("/_view/landingPage.jsp").forward(req, resp);	
-
 	}
 	
 }
