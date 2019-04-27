4b7d0282d1729bd748d0199689018bb33485d863
Madison Tibbett <Esherymack@users.noreply.github.com>
2019-04-27 00:16:29 -0400

    Implemented rough email sending

---
+++ CS320_Team_Fractal_Website/src/edu/ycp/cs320/CS320_Team_Fractal_Website/controller/pages/SmtpAuthenticator.java
@@ -0,0 +1,26 @@
+package edu.ycp.cs320.CS320_Team_Fractal_Website.controller.pages;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+
+public class SmtpAuthenticator extends Authenticator
+{
+	public SmtpAuthenticator()
+	{
+		super();
+	}
+	
+	@Override
+	public PasswordAuthentication getPasswordAuthentication()
+	{
+		String username = "320fractalsite@gmail.com";
+		String password = "***REMOVED***";
+		
+		if((username != null) && (username.length() > 0) && (password != null) && (password.length() > 0))
+		{
+			return new PasswordAuthentication(username, password);
+		}
+		return null;
+	}
+
+}
