72d9816b23e15ec5686b7068b078cab1e93c5489
Dakota Hilbert <dhilbert1@ycp.edu>
2019-05-02 22:52:55 -0400

    I updated the information for the tricorn fractal

--- CS320_Team_Fractal_Website/src/edu/ycp/cs320/CS320_Team_Fractal_Website/model/fractal/Tricorn.java
+++ CS320_Team_Fractal_Website/src/edu/ycp/cs320/CS320_Team_Fractal_Website/model/fractal/Tricorn.java
@@ -31,9 +31,9 @@ public class Tricorn extends Fractal{
 	
 	@Override
 	public String getInfo(){
-		return "The Mandelbrot set is the set of complex numbers for which the function does not diverge "
-				+ "when iterated from, i.e., for which the sequence, etc., remains bounded in absolute value. Its "
-				+ "definition and name are due to Adrien Douady, in tribute to the mathematician Benoit Mandelbrot.";
+		return "The Tricorn, often times called the Mandelbar set, is a fractal similar to the mandelbrot set. "
+				+ "The Tricorn uses the complex conjugate of the complex number in the equation, whereas the Mandelbrot only uses the complex number. "
+				+ "The fractal was introduced by W. D. Crowe, R. Hasson, P. J. Rippon, and P. E. D. Strain-Clark.";
 	}
 	
 	@Override
@@ -42,9 +42,9 @@ public class Tricorn extends Fractal{
 		return "<ul><li>Change X, Y values to zoom in and out on different regions.</li>"
 				+ "<li>Change multiplier to change the overall shape of the tricorn.</li>"
 				+ "<li>Example coordinates:</li>"
-				+ "<ul><li>X1: ; Y1: ; X2: ; Y2: ; M=1</li>"
-				+ "<li>X1: ; Y1: ; X2: ; Y2: ; M=1</li>"
-				+ "<li>X1: ; Y1: ; X2: ; Y2: ; M=1";
+				+ "<ul><li>X1: -1.5; Y1: -0.11; X2: -1.3; Y2: 0.09; M=1</li>"
+				+ "<li>X1: -1.27; Y1: -0.25; X2: -1.165; Y2: -0.145; M=1</li>"
+				+ "<li>X1: 0.175; Y1: -0.675; X2: 0.285; Y2: -0.515; M=1</li>";
 	}
 
 	@Override
